449f7b958577726d153c21c3aa31b1ec00d97af9
IvanSmolentseff <112988035+IvanSmolentseff@users.noreply.github.com>
2022-09-18 02:41:00 +0300

    Example 005 Condition If Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "ваня")
+{
+    Console.WriteLine("Рады Вас видеть, господин Иван!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
